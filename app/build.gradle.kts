9b2a93c5f4090f4b9b456ffe35f27f1c4c7bc479
Tobias D. <mail@tobiasdroste.de>
2025-07-12 10:10:40 +0200

    Version updates.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 android {
-    compileSdk = 34
+    compileSdk = 36
 
     buildFeatures {
         viewBinding = true
@@ -23,8 +23,8 @@ android {
     defaultConfig {
         applicationId = "com.tobiasdroste.papercups"
         minSdk = 23
-        targetSdk = 34
-        versionCode = 6
+        targetSdk = 36
+        versionCode = 8
         versionName = version as String
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         namespace = "com.tobiasdroste.papercups"
@@ -57,7 +57,7 @@ android {
 
 dependencies {
     implementation("javax.jmdns:jmdns:3.4.1")
-    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.preference:preference-ktx:1.2.1")
 
     androidTestImplementation("androidx.test.espresso:espresso-core:3.1.0-alpha4") {
@@ -66,18 +66,18 @@ dependencies {
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("junit:junit:4.13.2")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:2.0.21")
-    implementation("com.jakewharton.timber:timber:4.7.1")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:2.1.20")
+    implementation("com.jakewharton.timber:timber:5.0.1")
 
     // Koin DI
-    implementation("io.insert-koin:koin-android:3.5.3")
+    implementation("io.insert-koin:koin-android:4.1.0")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.1")
 
-    implementation("com.google.dagger:hilt-android:2.51.1")
-    ksp("com.google.dagger:hilt-android-compiler:2.51.1")
+    implementation("com.google.dagger:hilt-android:2.56.2")
+    ksp("com.google.dagger:hilt-android-compiler:2.56.2")
 
-    val roomVersion = "2.6.1"
+    val roomVersion = "2.7.2"
 
     implementation("androidx.room:room-runtime:$roomVersion")
     // optional - Kotlin Extensions and Coroutines support for Room
